3ad9339e13977ca9b7255cb7686da24df0276a99
KylianR <K.j.1998@live.nl>
2017-07-16 18:15:57 +0200

    Further tweaked sawn rates

--- Assets/Scritps/EnemySpawner.cs
+++ Assets/Scritps/EnemySpawner.cs
@@ -6,7 +6,7 @@ public class EnemySpawner : MonoBehaviour {
 
     BaseManager homeBase;
 
-    public int maxEnemies = 1000;
+    public int maxEnemies = 100;
     public float spawnRadius = 10;
 
     public GameObject enemy1;
@@ -34,11 +34,11 @@ public class EnemySpawner : MonoBehaviour {
 
             float enemychoice = Random.Range(0f, 2.4f);
             if(enemychoice < 1.5){
-                int count = Random.Range(12, 18);
-                for (int i = 0; i < count; i++)
+                int count = Random.Range(3, 4);
+                for (int i = 0; i < count && GameManager.enemyCount < maxEnemies; i++)
                 {
                     float loc = Random.Range(0f, 2 * 3.1415926535897932384626f);
-                    float dist = Random.Range(150.0f, 350.0f);
+                    float dist = Random.Range(50.0f, 90.0f);
                     Vector3 point = new Vector3(Mathf.Cos(loc), Mathf.Sin(loc)) * dist;
                     Vector3 location = transform.position + point;
                     Instantiate(enemy3, location, Quaternion.identity);
@@ -48,11 +48,11 @@ public class EnemySpawner : MonoBehaviour {
             
 
             if(enemychoice < 2 && enemychoice >= 1.5){
-                int count = Random.Range(7, 10);
-                for (int i = 0; i < count; i++)
+                int count = Random.Range(2, 3);
+                for (int i = 0; i < count && GameManager.enemyCount < maxEnemies; i++)
                 {
                     float loc = Random.Range(0f, 2 * 3.1415926535897932384626f);
-                    float dist = Random.Range(150.0f, 350.0f);
+                    float dist = Random.Range(40.0f, 120.0f);
                     Vector3 point = new Vector3(Mathf.Cos(loc), Mathf.Sin(loc)) * dist;
                     Vector3 location = transform.position + point;
                     Instantiate(enemy2, location, Quaternion.identity);
@@ -61,11 +61,11 @@ public class EnemySpawner : MonoBehaviour {
 
             if (enemychoice < 2.4 && enemychoice >= 2.0)
             {
-                int count = Random.Range(4, 9);
-                for (int i = 0; i < count; i++)
+                int count = Random.Range(1, 2);
+                for (int i = 0; i < count && GameManager.enemyCount < maxEnemies; i++)
                 {
                     float loc = Random.Range(0f, 2 * 3.1415926535897932384626f);
-                    float dist = Random.Range(50.0f, 350.0f);
+                    float dist = Random.Range(30.0f, 120.0f);
                     Vector3 point = new Vector3(Mathf.Cos(loc), Mathf.Sin(loc)) * dist;
                     Vector3 location = transform.position + point;
                     Instantiate(enemy1, location, Quaternion.identity);
@@ -102,7 +102,7 @@ public class EnemySpawner : MonoBehaviour {
                }
            } */
         }
-        yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        yield return new WaitForSeconds(Random.Range(.80f, 3.0f));
         yield return StartCoroutine(SpawnEnemies());
     }
 }
